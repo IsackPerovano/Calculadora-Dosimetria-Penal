91c24e6000d1c62329413a49910bcbf13aac8dda
IsacPerovano <perovanoisack@gmail.com>
2026-04-12 12:02:47 -0300

    atualizacao de paletas e finalizacao da estetica 1 fase

--- src/components/Cirscunstancias.tsx
+++ src/components/Cirscunstancias.tsx
@@ -2,7 +2,7 @@ import Icon from '../assets/justica.svg'
 
 export const Circunstancias = () => {
     return(
-        <div className="h-125 w-270 border-2 rounded-[15px] pl-3.5 border-[#d9d9d8] bg-white ml-50 mt-5">
+        <div className="h-115 w-250 border-2 rounded-[15px] pl-3.5 border-[#d9d9d8] bg-white ml-50 mt-5">
 
             <h3 
                className="flex pt-3 gap-3 text-[24px] poppins" style={{fontWeight: 500}} >
@@ -15,9 +15,9 @@ export const Circunstancias = () => {
                 Selecione as circunstâncias que devem ser valoradas.
             </h4>
 
-           <div className='flex pt-2'>
+           <div className='flex pt-2 items-center'>
                <h5 className='pr-1 poppins' style={{fontWeight: 400}} >Valor de cada Circunstancia:</h5> 
-               <input className='Border border-2 rounded-[10px] w-16.25 border-[#d9d9d8] text-center' type="number"/>     
+               <input className='Border border-2 rounded-[10px] w-16.25 border-[#d9d9d8] text-center' type="number" />     
                 <h5 className='text-[20px] pl-1 pr-1'> / </h5>
                 <input className='Border border-2 rounded-[10px] w-16.25 border-[#d9d9d8] text-center' type="number"/>     
            </div>
@@ -26,87 +26,135 @@ export const Circunstancias = () => {
 
                 <label className="cursor-pointer">
                     <input type="checkbox" className="peer hidden" />
-                    <div className="flex gap-4 items-center pl-3 border-2 rounded-[10px] py-3 w-130
+                    <div className="flex  items-center pl-3  rounded-[10px] h-17.5 w-120
                                     bg-[#1E2C4B] text-white
-                                    peer-checked:bg-[#D3DBEC] peer-checked:text-[#1E2C4B] peer-checked:border-[#D3DBEC] transition-colors duration-200">
+                                    peer-checked:bg-[#BCE7FD] peer-checked:text-[#1E2C4B] transition-colors duration-200">
                         <div className="flex flex-col">
-                        <h4 className="poppins font-bold">
-                            Culpabilidade
-                        </h4>
-                        <h5 className="poppins font-light">
-                            Grau de reprovabilidade da conduta.
-                        </h5>
+                            <h4 className="poppins font-bold">
+                                Culpabilidade
+                            </h4>
+                            <h5 className="poppins font-light">
+                                Grau de reprovabilidade da conduta.
+                            </h5>
+                        </div>
+                    </div>
+                </label>
+                
+                <label className="cursor-pointer">
+                    <input type="checkbox" className="peer hidden" />
+                    <div className="flex  items-center pl-3 rounded-[10px] h-17.5 w-120
+                                    bg-[#1E2C4B] text-white
+                                    peer-checked:bg-[#BCE7FD] peer-checked:text-[#1E2C4B] transition-colors duration-200">
+                        <div className="flex flex-col">
+                            <h4 className="poppins font-bold">
+                                Antecedentes
+                            </h4>
+                            <h5 className="poppins font-light">
+                                Vida pregressa do agente (condenações anteriores).
+                            </h5>
+                        </div>
+                    </div>
+                </label>
+              
+                <label className="cursor-pointer pt-1">
+                    <input type="checkbox" className="peer hidden" />
+                    <div className="flex gap-4 items-center pl-3  rounded-[10px] h-17.5 w-120
+                                    bg-[#1E2C4B] text-white
+                                    peer-checked:bg-[#BCE7FD] peer-checked:text-[#1E2C4B]  transition-colors duration-200">
+                        <div className="flex flex-col">
+                            <h4 className="poppins font-bold">
+                                Conduta Social
+                            </h4>
+                            <h5 className="poppins font-light">
+                                Relacionamento no meio familiar, trabalho e comunidade.
+                            </h5>
+                        </div>
+                    </div>
+                </label>
+               
+                <label className="cursor-pointer pt-1">
+                    <input type="checkbox" className="peer hidden" />
+                    <div className="flex  items-center pl-3  rounded-[10px] h-17.5 w-120
+                                    bg-[#1E2C4B] text-white
+                                    peer-checked:bg-[#BCE7FD] peer-checked:text-[#1E2C4B]  transition-colors duration-200">
+                        <div className="flex flex-col">
+                            <h4 className="poppins font-bold">
+                                Personalidade
+                            </h4>
+                            <h5 className="poppins font-light">
+                               Índole e caráter do agente.
+                            </h5>
+                        </div>
+                    </div>
+                </label>
+               
+                <label className="cursor-pointer pt-1">
+                    <input type="checkbox" className="peer hidden" />
+                    <div className="flex  items-center pl-3  rounded-[10px] h-17.5 w-120
+                                    bg-[#1E2C4B] text-white
+                                    peer-checked:bg-[#BCE7FD] peer-checked:text-[#1E2C4B] transition-colors duration-200">
+                        <div className="flex flex-col">
+                            <h4 className="poppins font-bold">
+                                Motivos
+                            </h4>
+                            <h5 className="poppins font-light">
+                               Razões que levaram à prática do crime.
+                            </h5>
+                        </div>
+                    </div>
+                </label>
+                
+                <label className="cursor-pointer pt-1">
+                    <input type="checkbox" className="peer hidden" />
+                    <div className="flex  items-center pl-3  rounded-[10px] h-17.5 w-120
+                                    bg-[#1E2C4B] text-white
+                                    peer-checked:bg-[#BCE7FD] peer-checked:text-[#1E2C4B] transition-colors duration-200">
+                        <div className="flex flex-col">
+                            <h4 className="poppins font-bold">
+                                Circunstâncias
+                            </h4>
+                            <h5 className="poppins font-light">
+                               Lugar, tempo e modo de execução.
+                            </h5>
+                        </div>
+                    </div>
+                </label>
+
+                <label className="cursor-pointer pt-1">
+                    <input type="checkbox" className="peer hidden" />
+                    <div className="flex  items-center pl-3  rounded-[10px] h-17.5 w-120
+                                    bg-[#1E2C4B] text-white
+                                    peer-checked:bg-[#BCE7FD] peer-checked:text-[#1E2C4B]  transition-colors duration-200">
+                        <div className="flex flex-col">
+                            <h4 className="poppins font-bold">
+                                Consequências
+                            </h4>
+                            <h5 className="poppins font-light">
+                               Danos causados pelo crime.
+                            </h5>
+                        </div>
+                    </div>
+                </label>
+
+                <label className="cursor-pointer pt-1">
+                    <input type="checkbox" className="peer hidden" />
+                    <div className="flex items-center pl-3  rounded-[10px] h-17.5 w-120
+                                    bg-[#1E2C4B] text-white
+                                    peer-checked:bg-[#BCE7FD] peer-checked:text-[#1E2C4B]  transition-colors duration-200">
+                        <div className="flex flex-col">
+                            <h4 className="poppins font-bold">
+                                Comportamento da Vítima
+                            </h4>
+                            <h5 className="poppins font-light">
+                               Se a vítima contribuiu para o crime.
+                            </h5>
                         </div>
                     </div>
                 </label>
 
 
 
-                <label className='cursor-pointer'>
-                    <input type="checkbox" className='peer hidden'/>
-                    <div className='flex gap-4 items-center pl-3 border-2 rounded-[10px] py-3 w-130
-                                    bg-[#1E2C4B] text-white
-                                    peer-checked:bg-[#D3DBEC] peer-checked:text-[#1E2C4B] peer-checked:border-[#D3DBEC] transition-colors duration-200'>
-                        <h4 className=' poppins' style={{fontWeight:700}}> Conduta Social </h4>
-                        <h5 className='poppins' style={{fontWeight:300}}> Relacionamento no meio familiar, trabalho e comunidade. </h5>
-                    </div>
-                    </label>
-
-               <div className='bg-[#1E2C4B] text-white border-2 rounded-[10px] h-18.75 w-130 flex items-center'>
-                    <label className='flex gap-4 items-start pl-3'>
-                        <input type="checkbox" className='mt-2'/>
-                        <div className='flex flex-col'>
-                            <h4 className=' poppins' style={{fontWeight:700}}> Motivos </h4>
-                            <h5 className='poppins' style={{fontWeight:300}}> Razões que levaram à prática do crime. </h5>
-                        </div>
-                    </label>
-                </div> 
-               <div className='bg-[#1E2C4B] text-white border-2 rounded-[10px] h-18.75 w-130 flex items-center'>
-                    <label className='flex gap-4 items-start pl-3'>
-                        <input type="checkbox" className='mt-2'/>
-                        <div className='flex flex-col'>
-                            <h4 className=' poppins' style={{fontWeight:700}}> Consequências </h4>
-                            <h5 className='poppins' style={{fontWeight:300}}> Danos causados pelo crime. </h5>
-                        </div>
-                    </label>
-                </div> 
-               <div className='bg-[#1E2C4B] text-white border-2 rounded-[10px] h-18.75 w-130 flex items-center'>
-                    <label className='flex gap-4 items-start pl-3'>
-                        <input type="checkbox" className='mt-2'/>
-                        <div className='flex flex-col'>
-                            <h4 className=' poppins' style={{fontWeight:700}}> Antecedentes </h4>
-                            <h5 className='poppins' style={{fontWeight:300}}> Vida pregressa do agente (condenações anteriores). </h5>
-                        </div>
-                    </label>
-                </div> 
-               <div className='bg-[#1E2C4B] text-white border-2 rounded-[10px] h-18.75 w-130 flex items-center'>
-                    <label className='flex gap-4 items-start pl-3'>
-                        <input type="checkbox" className='mt-2'/>
-                        <div className='flex flex-col'>
-                            <h4 className=' poppins' style={{fontWeight:700}}> Personalidade </h4>
-                            <h5 className='poppins' style={{fontWeight:300}}> Índole e caráter do agente. </h5>
-                        </div>
-                    </label>
-                </div> 
-               <div className='bg-[#1E2C4B] text-white border-2 rounded-[10px] h-18.75 w-130 flex items-center'>
-                    <label className='flex gap-4 items-start pl-3'>
-                        <input type="checkbox" className='mt-2'/>
-                        <div className='flex flex-col'>
-                            <h4 className=' poppins' style={{fontWeight:700}}> Circunstâncias </h4>
-                            <h5 className='poppins' style={{fontWeight:300}}> Grau de reprovabilidade da conduta. </h5>
-                        </div>
-                    </label>
-                </div> 
-               <div className='bg-[#1E2C4B] text-white border-2 rounded-[10px] h-18.75 w-130 flex items-center'>
-                    <label className='flex gap-4 items-start pl-3'>
-                        <input type="checkbox" className='mt-2'/>
-                        <div className='flex flex-col'>
-                            <h4 className=' poppins' style={{fontWeight:700}}> Comportamento da Vítima </h4>
-                            <h5 className='poppins' style={{fontWeight:300}}> Se a vítima contribuiu para o crime. </h5>
-                        </div>
-                    </label>
-                </div> 
-
+                
 
            </div>
 
--- src/components/Fases.tsx
+++ src/components/Fases.tsx
@@ -5,21 +5,21 @@ export const Fases = () => {
         <div className="Border border-2 border-[#d9d9d8] rounded-[20px] flex mt-2.5 ml-50 w-250 h-15 p-1 gap-1 poppins bg-white" style={{fontWeight: 400}}>
 
                 <input 
-                    className="flex flex-auto bg-[#1E2C4B] hover:bg-[#D3DBEC] hover:text-[#1E2C4B] border border-transparent hover:border-[#d9d9d8] 
+                    className="flex flex-auto bg-[#1E2C4B] hover:bg-[#BCE7FD] hover:text-[#1E2C4B] border border-transparent hover:border-[#d9d9d8] 
                         transition-colors duration-200 rounded-[15px] text-white" 
                     type="button" 
                     value="1° Fase" 
                 />
 
                 <input 
-                    className="flex flex-auto bg-[#1E2C4B] hover:bg-[#D3DBEC] hover:text-[#1E2C4B] border border-transparent hover:border-[#d9d9d8] 
+                    className="flex flex-auto bg-[#1E2C4B] hover:bg-[#BCE7FD] hover:text-[#1E2C4B] border border-transparent hover:border-[#d9d9d8] 
                         transition-colors duration-200 rounded-[15px] text-white" 
                     type="button" 
                     value="2° Fase" 
                 />
 
                 <input 
-                    className="flex flex-auto bg-[#1E2C4B] hover:bg-[#D3DBEC] hover:text-[#1E2C4B] border border-transparent hover:border-[#d9d9d8] 
+                    className="flex flex-auto bg-[#1E2C4B] hover:bg-[#BCE7FD] hover:text-[#1E2C4B] border border-transparent hover:border-[#d9d9d8] 
                         transition-colors duration-200 rounded-[15px] text-white" 
                     type="button" 
                     value="3° Fase" 
